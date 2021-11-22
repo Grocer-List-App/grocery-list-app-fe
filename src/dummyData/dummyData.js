2f9aba3add3383f734d666d1325644d214d96658
Bradly <bradlylewisdev@gmail.com>
2021-11-21 21:06:26 -0500

    add reducers actions and dummyData

---
+++ src/dummyData/dummyData.js
@@ -0,0 +1,153 @@
+const dummyData = [{
+    "id": 1,
+    "item": "Olives - Morracan Dired"
+  }, {
+    "id": 2,
+    "item": "Spic And Span All Purpose"
+  }, {
+    "id": 3,
+    "item": "Soup - Knorr, Country Bean"
+  }, {
+    "id": 4,
+    "item": "Sausage - Liver"
+  }, {
+    "id": 5,
+    "item": "Tortillas - Flour, 10"
+  }, {
+    "id": 6,
+    "item": "Pears - Anjou"
+  }, {
+    "id": 7,
+    "item": "Coffee Caramel Biscotti"
+  }, {
+    "id": 8,
+    "item": "Steampan Lid"
+  }, {
+    "id": 9,
+    "item": "Pepper - Black, Whole"
+  }, {
+    "id": 10,
+    "item": "Wine - White, Riesling, Semi - Dry"
+  }, {
+    "id": 11,
+    "item": "Sage Derby"
+  }, {
+    "id": 12,
+    "item": "Cheese - Gouda"
+  }, {
+    "id": 13,
+    "item": "Olives - Morracan Dired"
+  }, {
+    "id": 14,
+    "item": "Crab - Meat"
+  }, {
+    "id": 15,
+    "item": "Muffin Mix - Chocolate Chip"
+  }, {
+    "id": 16,
+    "item": "Mushroom - Chanterelle, Dry"
+  }, {
+    "id": 17,
+    "item": "Buttons"
+  }, {
+    "id": 18,
+    "item": "Relish"
+  }, {
+    "id": 19,
+    "item": "Quail Eggs - Canned"
+  }, {
+    "id": 20,
+    "item": "Yeast - Fresh, Fleischman"
+  }, {
+    "id": 21,
+    "item": "Apple - Macintosh"
+  }, {
+    "id": 22,
+    "item": "Cream Of Tartar"
+  }, {
+    "id": 23,
+    "item": "Ham - Cooked Bayonne Tinned"
+  }, {
+    "id": 24,
+    "item": "Danishes - Mini Raspberry"
+  }, {
+    "id": 25,
+    "item": "Lid - 3oz Med Rec"
+  }, {
+    "id": 26,
+    "item": "Spice - Montreal Steak Spice"
+  }, {
+    "id": 27,
+    "item": "Passion Fruit"
+  }, {
+    "id": 28,
+    "item": "The Pop Shoppe - Grape"
+  }, {
+    "id": 29,
+    "item": "Quiche Assorted"
+  }, {
+    "id": 30,
+    "item": "Compound - Rum"
+  }, {
+    "id": 31,
+    "item": "Tray - Foam, Square 4 - S"
+  }, {
+    "id": 32,
+    "item": "Cloves - Ground"
+  }, {
+    "id": 33,
+    "item": "Aspic - Light"
+  }, {
+    "id": 34,
+    "item": "Chips - Miss Vickies"
+  }, {
+    "id": 35,
+    "item": "Chips - Assorted"
+  }, {
+    "id": 36,
+    "item": "Broom - Push"
+  }, {
+    "id": 37,
+    "item": "Chicken - Leg, Boneless"
+  }, {
+    "id": 38,
+    "item": "Wine - White, Antinore Orvieto"
+  }, {
+    "id": 39,
+    "item": "Jameson Irish Whiskey"
+  }, {
+    "id": 40,
+    "item": "Puree - Kiwi"
+  }, {
+    "id": 41,
+    "item": "Pimento - Canned"
+  }, {
+    "id": 42,
+    "item": "Fib N9 - Prague Powder"
+  }, {
+    "id": 43,
+    "item": "Vermacelli - Sprinkles, Assorted"
+  }, {
+    "id": 44,
+    "item": "Crackers - Graham"
+  }, {
+    "id": 45,
+    "item": "Cucumber - English"
+  }, {
+    "id": 46,
+    "item": "V8 - Tropical Blend"
+  }, {
+    "id": 47,
+    "item": "Lettuce - Escarole"
+  }, {
+    "id": 48,
+    "item": "Asparagus - Mexican"
+  }, {
+    "id": 49,
+    "item": "Mushroom - Porcini Frozen"
+  }, {
+    "id": 50,
+    "item": "Boogies"
+  }]
+
+  export default dummyData;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
 import './index.css';
 import App from './App';
+
+const store = createStore(applyMiddleware(logger, thunk))
 
 ReactDOM.render(
   <React.StrictMode>
