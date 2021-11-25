7af335e59ef36bcde7222b8b79dfecb8a7887b7f
Bradly <bradlylewisdev@gmail.com>
2021-11-25 13:27:15 -0500

    added NavBar component and imported it

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import '../App.css';
+import NavBar from './Common/NavBar.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Header
+        <NavBar />
       </header>
       <h1>Grocery List App</h1>
     </div>
---
+++ src/Components/Common/NavBar.js
@@ -0,0 +1,10 @@
+import { NavLink } from 'react-router-dom';
+
+export default function NavBar() {
+    return (
+        <div>
+            <NavLink>Nav Bar</NavLink>
+        </div>
+    )
+
+}
