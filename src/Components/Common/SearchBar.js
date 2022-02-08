7d2fe635d09c5060c4d29915729d755f1dc62ea4
Bradly <bradlylewisdev@gmail.com>
2022-02-08 15:10:52 -0500

    create functional filter search bar

--- src/Components/Common/SearchBar.js
+++ src/Components/Common/SearchBar.js
@@ -1,30 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './Common.module.css';
 import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
 
-export default function SearchBar({placeholder, data}) {
-  const [ filteredData, setFilteredData ] = useState([]);
-
-  const handleSearch = (e) => {
-    const searchWord = e.target.value.toLowerCase();
-    const newFilter = data.filter(value => {
-      return value.name.toLowerCase().includes(searchWord);
-    });
-    setFilteredData(newFilter);
-  }
+export default function SearchBar({ placeholder, handleSearch, clearInput, state }) {
 
   return (
     <div className={styles.search}>
       <div className={styles.search_inputs}>
-        <input type="text" placeholder={placeholder} onChange={handleSearch}/>
-        <div className={styles.search_icon}><SearchIcon /></div>
-      </div>
-      <div className={styles.search_result}>
-        {filteredData.map((item, index) => {
-          return (
-            <h1>{item.name}</h1>
-        )})}
+        <input type="text"
+          placeholder={placeholder}
+          onChange={handleSearch}
+          name='enteredWord'
+          value={state.enteredWord}
+        />
+        <div className={styles.search_icon}>
+          {state.enteredWord === '' ? <SearchIcon /> : <CloseIcon id={styles.close_button} onClick={clearInput}/>}
         </div>
+      </div>
     </div>
 );
 }
--- src/Components/Pages/Recipes/Recipes.js
+++ src/Components/Pages/Recipes/Recipes.js
@@ -1,16 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Recipes.module.css';
 import SearchBar from '../../Common/SearchBar';
 import Data from '../../../Data.json';
 
 export default function Recipes() {
+  const [ filteredData, setFilteredData ] = useState(Data);
+  const [ state, setState ] = useState({enteredWord: ''});
+
+  const handleSearch = (e) => {
+    const searchWord = e.target.value.toLowerCase();
+    const newFilter = Data.filter(item => {
+      return item.name.toLowerCase().includes(searchWord);
+    });
+    searchWord.length > 0 ? setFilteredData(newFilter) : setFilteredData(Data);
+    setState({...state, [e.target.name]: e.target.value});
+  }
+
+  const clearInput = () => {
+    setFilteredData(Data);
+    setState({enteredWord: ''});
+  }
 
   return (
-    <div className={styles.recipes_container}>
-        <div className={styles.user_banner}>Bradly's Recipes</div>
-        <div className={styles.filter_bar}>
-            <SearchBar className={styles.search_bar} placeholder='Search for recipes...' data={Data} />
-        </div>
+    <div className={styles.recipes_wrapper}>
+      <div className={styles.user_banner}>Bradly's Recipes</div>
+      <div className={styles.filter_bar}>
+        <SearchBar className={styles.search_bar}
+          placeholder='Search for recipes...'
+          data={filteredData}
+          handleSearch={handleSearch}
+          clearInput={clearInput}
+          state={state}
+        />
+      </div>
+      <div className={styles.recipes_container}>
+        {filteredData.map((item, index) => {
+          return (
+            <div key={index} className={styles.recipe_card}>{item.name}</div>
+          )
+        })}
+      </div>
     </div>
   );
 }
