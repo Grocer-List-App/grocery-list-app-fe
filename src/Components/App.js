32a0c6562973275d97223c495c1ef6a1d6453ae2
Bradly Lewis <bradlylewisdev@gmail.com>
2022-10-13 04:53:43 -0400

    add mui navbar and update links

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,18 +1,15 @@
-import { Route, Routes } from 'react-router-dom';
-import NavBar from './Common/NavBar.js';
-import HomePage from './Pages/HomePage.js';
+import * as React from 'react';
+import NavBar from './Common/NavBar';
 
 import '../App.css';
 
 function App() {
   return (
-      <div id="body-wrapper">
+      <div>
         <NavBar />
-        <Routes>
-            <Route exact path="/" element={<HomePage/>} />
-        </Routes>
       </div>
   );
 }
 
 export default App;
+
